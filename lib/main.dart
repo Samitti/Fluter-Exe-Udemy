e6186aafb389eb9ad1feb7a2f0ba822bac379f16
Samitti <samuelteweldebrhan@gmail.com>
2021-10-21 11:27:32 +0300

    finish quiz app

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 
+import './quiz.dart';
+import './result.dart';
+
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatefulWidget {
-
   @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
@@ -13,44 +15,63 @@ class MyApp extends StatefulWidget {
   }
 }
 
+class MyAppState extends State<MyApp> {  
 
-class MyAppState extends State<MyApp> {
-  var index = 0;
-  var questions = [
-      'Whats your ?',
-      'How old are you?',
-    ];
-  void changeQuestion () {
+  var _questions = [
+    {
+      'questionText': 'What\'s your favorite color?',
+      'answer': [
+        {'text': 'Black', 'score': 10},
+        {'text': 'Red', 'score': 5},
+        {'text': 'Green', 'score': 3},
+        {'text': 'White', 'score': 1}
+      ],
+    },
+    {
+      'questionText': 'What\s your favorite animal?',
+      'answer': [
+        {'text': 'Lion', 'score': 10},
+        {'text': 'Elephant', 'score': 5},
+        {'text': 'Ducs', 'score': 3},
+        {'text': 'Cat', 'score': 1}
+        ],
+    },
+    {
+      'questionText': 'What\s your favorite Teacheral?',
+      'answer': [
+        {'text': 'Max', 'score': 10},
+        {'text': 'Alex', 'score': 5},
+        {'text': 'Sam', 'score': 3},
+        {'text': 'Ninja', 'score': 1}
+        ],
+    }
+  ];
+
+  var _questionIndex = 0;
+  var _totalScore = 0;
+
+  void _answerQuestion(int score) {
+    _totalScore = _totalScore + score;
+
     setState(() {
-      index = index + 1;      
+      _questionIndex = _questionIndex + 1;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My App'),
         ),
-        body: Column(
-          children: [
-            Text(questions[index]),
-            RaisedButton(
-              onPressed: changeQuestion,
-              child: Text('Button 1'),
-            ),
-            RaisedButton(
-              onPressed: changeQuestion,
-              child: Text('Button 1'),
-            ),
-            RaisedButton(
-              onPressed: changeQuestion,
-              child: Text('Button 1'),
-            ),
-          ],
-        ),
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                answerQuestions: _answerQuestion,
+                questionIndex: _questionIndex,
+                questions: _questions,
+              )
+            : Result(_totalScore),
       ),
     );
   }
